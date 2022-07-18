468eb461f374059bf4bef889a8bfcdb3fe79908e
Sychova Ksenia <mlichlundiapl@gmail.com>
2022-07-18 15:44:29 +0200

    US002: added base urls for auth

---
+++ src/app/constants.ts
@@ -0,0 +1,6 @@
+export const BASE_URL_SIGNIN =
+  'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=';
+export const BASE_URL_SIGNUP =
+  'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=';
+export const BASE_URL_GET_USER =
+  'https://identitytoolkit.googleapis.com/v1/accounts:createAuthUri?key=';
