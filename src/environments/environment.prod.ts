4d1ffa21a332ec7d2cc56e65ad06c18a51021fc8
Sychova Ksenia <mlichlundiapl@gmail.com>
2022-07-18 15:42:53 +0200

    US002: updated enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: 'AIzaSyCo6gmvdkBWwQaXOC5YK9XED1suOLy88eg',
+  dataBase: 'https://super-heroes-api-e24a5-default-rtdb.firebaseio.com/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,12 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
+import { Enviroment } from './interface';
+
+export const environment: Enviroment = {
+  production: false,
+  apiKey: 'AIzaSyCo6gmvdkBWwQaXOC5YK9XED1suOLy88eg',
+  dataBase: 'https://super-heroes-api-e24a5-default-rtdb.firebaseio.com/',
 };
 
 /*
