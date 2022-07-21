ac2fcf19f969c415cc958f200383a0350c6ad24a
Sychova Ksenia <mlichlundiapl@gmail.com>
2022-07-21 18:15:44 +0200

    US004: fixed code style

--- src/app/pages/main-page/pages/user-info/user-info.component.ts
+++ src/app/pages/main-page/pages/user-info/user-info.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user-info',
   templateUrl: './user-info.component.html',
-  styleUrls: ['./user-info.component.scss']
+  styleUrls: ['./user-info.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserInfoComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
