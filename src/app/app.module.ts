a63c9a546fc1a77a24006277191c4479e4234cf2
Sychova Ksenia <mlichlundiapl@gmail.com>
2022-07-21 15:21:09 +0200

    US004: added interseptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,17 @@
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgModule, Provider } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthInterseptor } from './auth.interseptor';
+
+const INTERSEPTOR_PROVIDER: Provider = {
+  provide: HTTP_INTERCEPTORS,
+  multi: true,
+  useClass: AuthInterseptor,
+};
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,7 +22,7 @@ import { AppComponent } from './app.component';
     ReactiveFormsModule,
     HttpClientModule,
   ],
-  providers: [],
+  providers: [INTERSEPTOR_PROVIDER],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
