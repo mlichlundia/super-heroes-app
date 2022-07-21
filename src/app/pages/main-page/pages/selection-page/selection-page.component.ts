738f241d93d29d616de38d1179e20b8eef7e226b
Sychova Ksenia <mlichlundiapl@gmail.com>
2022-07-21 18:16:31 +0200

    US004: got heroes from database

--- src/app/pages/main-page/pages/selection-page/selection-page.component.ts
+++ src/app/pages/main-page/pages/selection-page/selection-page.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Hero } from 'src/app/interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-selection-page',
   templateUrl: './selection-page.component.html',
-  styleUrls: ['./selection-page.component.scss']
+  styleUrls: ['./selection-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SelectionPageComponent implements OnInit {
+export class SelectionPageComponent {
+  public heroes!: Observable<Hero[]>;
 
-  constructor() { }
+  constructor(public _heroService: HeroesService) {}
 
-  ngOnInit(): void {
+  public submit(searchValue: string) {
+    this.heroes = this._heroService.getHeroes(searchValue);
   }
-
 }
