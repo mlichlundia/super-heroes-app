864d1a150f243bdb4d36f9219f3e0b060d799044
Sychova Ksenia <mlichlundiapl@gmail.com>
2022-07-21 18:14:36 +0200

    US004: added new declarations

--- src/app/pages/main-page/main.module.ts
+++ src/app/pages/main-page/main.module.ts
@@ -6,9 +6,13 @@ import { AuthGuard } from 'src/app/auth.guard';
 import { MainRoutingModule } from './main-routing.module';
 import { UserInfoComponent } from './pages/user-info/user-info.component';
 import { SelectionPageComponent } from './pages/selection-page/selection-page.component';
+import { HeroCardComponent } from './components/hero-card/hero-card.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { PreviousSearchesComponent } from './components/previous-searches/previous-searches.component';
+import { SearchPointComponent } from './components/search-point/search-point.component';
 
 @NgModule({
-  declarations: [UserInfoComponent, SelectionPageComponent],
+  declarations: [UserInfoComponent, SelectionPageComponent, HeroCardComponent, SearchBarComponent, PreviousSearchesComponent, SearchPointComponent],
   imports: [
     CommonModule,
     MainRoutingModule,
