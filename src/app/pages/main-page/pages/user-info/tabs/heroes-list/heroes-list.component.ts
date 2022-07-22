0f36ebcfdf7d8cee2986af1338db3e424b0a10e7
Sychova Ksenia <mlichlundiapl@gmail.com>
2022-07-22 15:17:45 +0200

    US007: added heroes-list view and logic

--- src/app/pages/main-page/pages/user-info/tabs/heroes-list/heroes-list.component.ts
+++ src/app/pages/main-page/pages/user-info/tabs/heroes-list/heroes-list.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from 'src/app/interfaces/hero.interface';
 
 @Component({
   selector: 'app-heroes-list',
   templateUrl: './heroes-list.component.html',
   styleUrls: ['./heroes-list.component.scss'],
 })
-export class HeroesListComponent {}
+export class HeroesListComponent implements OnInit {
+  public heroes: Hero[] = JSON.parse(localStorage.getItem('my-heroes')!);
+  public selected!: Hero;
+
+  public ngOnInit(): void {
+    this._showHeroes();
+    this._setSelected();
+  }
+
+  private _showHeroes(): void {
+    this.heroes = JSON.parse(localStorage.getItem('my-heroes')!);
+  }
+
+  private _setSelected() {
+    this.selected = this.heroes[this.heroes.length - 1];
+  }
+}
