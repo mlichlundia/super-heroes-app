7810ead7316bf2b6178f9a93cd1df30bd4dc8828
Sychova Ksenia <mlichlundiapl@gmail.com>
2022-07-26 13:27:47 +0200

    US011: created pop-up component

---
+++ src/app/pages/main-page/components/pop-up/pop-up.component.ts
@@ -0,0 +1,20 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Output,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-pop-up',
+  templateUrl: './pop-up.component.html',
+  styleUrls: ['./pop-up.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PopUpComponent {
+  @Output() public close: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  public closePopUp(): void {
+    this.close.emit(false);
+  }
+}
